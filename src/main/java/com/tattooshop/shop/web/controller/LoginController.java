5b54a96e4db1b5e101dbae92f05d1ea8bdbcd61a
katjaprodanova <katja.prodanova.001@gmail.com>
2022-04-21 17:31:52 +0200

    commit

--- src/main/java/com/tattooshop/shop/web/controller/LoginController.java
+++ src/main/java/com/tattooshop/shop/web/controller/LoginController.java
@@ -34,7 +34,7 @@ public class LoginController {
             user = this.authService.login(request.getParameter("username"),
                     request.getParameter("password"));
             request.getSession().setAttribute("user", user);
-            return "register";
+            return "redirect:/home";
         }
         catch (InvalidUsernameOrPasswordException exception) {
             model.addAttribute("hasError", true);
